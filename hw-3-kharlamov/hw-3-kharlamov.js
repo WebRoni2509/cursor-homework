93d683c6113a35937dbd3ac3ae7816a2aeecc4de
WebRoni2509 <webroni2509@gmail.com>
2020-07-25 09:46:36 +0300

    'homework_3_backup'

---
+++ hw-3-kharlamov/hw-3-kharlamov.js
@@ -0,0 +1,59 @@
+
+// Get Max Digit function
+const getMaxDigit = (digit) => {
+  digit = (digit + '').split('').sort();
+  return `Найбільша цифра з числа ${(digit).join('')} = ${digit[digit.length - 1]}`  
+}
+
+document.writeln(`Функція №1 - отримати найбільшу цифру з числа ${getMaxDigit(765567221833)}`)
+
+// 2
+const degreeOfNum = (num, deg ) => {
+    let res = 1;
+    for(let i = 0; i < deg; i++)
+    {
+        res = res * num;
+    }
+    return res;
+}
+console.log(degreeOfNum(2, 2)); 
+
+// 3
+const capitalizeWord = (name) => {
+  return name.charAt(0).toUpperCase() + name.slice(1);
+}
+console.log(capitalizeWord('roman'));
+
+//5
+const getRandomNubmer = (fromNumber, toNubmer) => {
+  return Math.floor(Math.random() * (toNubmer - fromNumber + 1) + fromNumber);
+}
+console.log(getRandomNubmer(1, 10));
+
+//6
+const countLetters = (letterToFind, wordToCheck) => {
+  let lettersCounter = 0;
+  for(let i = 0; i < wordToCheck.length; i++){
+    if(letterToFind === wordToCheck[i])
+      lettersCounter++
+  }
+  return lettersCounter
+}
+console.log(countLetters('s','aasasdsad'));
+
+//9 
+const deleteLetters = (letterToDelete, wordToCheck) => {
+
+  for(let i = 0; i < wordToCheck.length; i++){
+    wordToCheck = wordToCheck.replace(letterToDelete, '')
+  }
+  return wordToCheck;
+}
+console.log(deleteLetters('l','hello world'));
+
+
+
+
+
+
+
