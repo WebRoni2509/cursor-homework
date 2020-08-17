2259434bc03ad044c6c55f4a77fa0320692758d2
WebRoni2509 <webroni2509@gmail.com>
2020-08-17 15:09:06 +0300

    'homework_9_for_review'

--- hw-9-kharlamov/hw-9-kharlamov.js
+++ hw-9-kharlamov/hw-9-kharlamov.js
@@ -1,1 +1,38 @@
 
+const styles = {
+  width: '50px',
+  height: '50px',
+  display: 'inline-block'
+}
+
+const blocksContainer = document.querySelector('.container');
+const runIntervalBtn = document.querySelector('#run-interval');
+
+const getRandomColor = () => {
+	return '#'+Math.floor(Math.random()*16777215).toString(16);
+}
+
+const createBlock = () => {
+	const block = document.createElement("div");
+	blocksContainer.appendChild(block);
+	block.classList.add('block-item');
+	block.style.width = styles.width;
+	block.style.height = styles.height;
+	block.style.display = styles.display;
+	block.style.background = getRandomColor();
+}
+
+const generateBlocks = () => {
+	for(let i = 0; i < 25; i++){
+  	createBlock();
+  } 
+}
+
+const generateBlocksInterval = () => {
+	const allBlocks = document.querySelectorAll('.block-item');
+	allBlocks.forEach(function (element) {
+		element.style.backgroundColor = getRandomColor();
+	})
+}
+generateBlocks();
+runIntervalBtn.addEventListener('click', setInterval(generateBlocksInterval, 1000));
