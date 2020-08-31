e075ba06e3307750f5a27454b09acfbc6e7364a8
WebRoni2509 <webroni2509@gmail.com>
2020-08-31 17:48:10 +0300

    'hw_13_backup'

--- hw-13-kharlamov/main.js
+++ hw-13-kharlamov/main.js
@@ -12,3 +12,15 @@ const idGenerator = createIdGenerator();
 generateIdBtn.addEventListener('click', () => {
   return idResult.innerHTML = idGenerator.next().value
 })
+
+//advanced
+
+// function* newFontGenerator(fontSize){
+//   fontSize = 14;
+//   if(fontSize === 'up'){
+//     yield fontSize = fontSize + 2;
+//   }else if(fontSize === 'down'){
+//     yield fontSize = fontSize - 2;
+//   }else{return}
+// }
+
